8349f7f7e0f56762588b8417f2cfb8597c0492b5
Natan Alves <108841058+natanignacioalves@users.noreply.github.com>
2022-10-28 22:22:00 -0300

    update

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
+
+const validateInput = ({target}) => { //desestruturaçao de objts
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled');
+        return;
+    }
+    button.setAttribute('disabled', '');
+}
+//------------------------------------------------- ok
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value); //salva valor inserido no localSorage do navegador
+    window.location = 'pages/game.html'; //redireciona
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit' , handleSubmit);
